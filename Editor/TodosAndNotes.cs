3e75591e009b76ddadcd920a210cbf99a42bc4ba
Glaas <s.decuyper314@gmail.com>
2021-04-02 22:53:51 +0200

    Adjusted version on package.json
    HUGE breakthrough, see commit description
    
    - Finally understood what the deal is with Assembly Definitions
    - Package BUILDS FINALLY
    - Rough WORKING version of SebTodosLists
    - Namespaces needs to be redefined
    - Finally understood the main difference between Runtime and Editor folders, put script in correct ones
    - etc...

---
+++ Editor/TodosAndNotes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Todo list", menuName = "Todo list")]
+public class TodosAndNotes : ScriptableObject
+{
+    public string title;
+    public string content;
+    public List<String> todos;
+
+    public void Init(string title, string content, List<string> todos)
+    {
+        this.title = title;
+        this.content = content;
+        this.todos = todos;
+    }
+    
+}
